a2e6529c507fa90205fe901b5cf431dd0f909b9e
Greg Smyth <grsmyth@deloitte.ca>
2020-12-28 16:49:33 -0500

    Testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const webpack = require('webpack');
-const dotenv = require('dotenv').config({ path: `${__dirname}/.env` });
 
 /* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
 module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({ 'global.GENTLY': false }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(dotenv.parsed),
-    }),
-  ],
+  plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })],
   node: {
     __dirname: true,
   },
